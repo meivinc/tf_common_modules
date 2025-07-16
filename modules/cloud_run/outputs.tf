2b5f303bff8ec84f29e59a5e0faca64bb8834c2c
meivinc <vincent.meindl@gmail.com>
2025-07-16 09:08:48 +0200

    fix(cloud-run): update module output

--- modules/cloud_run/outputs.tf
+++ modules/cloud_run/outputs.tf
@@ -1,15 +1,15 @@
 
 output "service_name" {
-  value       = google_cloud_run_service.main.name
+  value       = google_cloud_run_v2_service.default.name
   description = "Name of the created service"
 }
 
 output "revision" {
-  value       = google_cloud_run_service.main.status[0].latest_ready_revision_name
+  value       = google_cloud_run_v2_service.default.latest_ready_revision.name
   description = "Deployed revision for the service"
 }
 
 output "service_url" {
-  value       = google_cloud_run_service.main.status[0].url
+  value       = google_cloud_run_v2_service.default.urls
   description = "The URL on which the deployed service is available"
 }
